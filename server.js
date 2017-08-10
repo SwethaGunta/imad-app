7690b40af84e78f4c8f285180fd425909797d7ba
SwethaGunta <SwethaGunta@users.noreply.github.com>
2017-08-10 07:35:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
+    console.log(articleName);
     res.send(createTemplate(articles[articleName]));
     
 });
