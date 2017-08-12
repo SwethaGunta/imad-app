9bd71f72c3a8523d730ee4c12f7b63236151210f
SwethaGunta <SwethaGunta@users.noreply.github.com>
2017-08-12 11:18:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 
-
+app.get('/:articleName',function(req,res){
+    console.log(req.params.articleName);
+    var articleName = req.params.articleName;
+    console.log(articleName);
+    res.send(createTemplate(articles[articleName]));
+});
 
 
 
