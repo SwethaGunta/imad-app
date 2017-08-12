7cdafeeab98ec34ff29067aa35eb33d418ccc3a4
SwethaGunta <SwethaGunta@users.noreply.github.com>
2017-08-12 16:04:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,6 +39,12 @@ var articles =
   date: 'Sept 10,2017',
   author:'author 2'}};
 
+var names=[];
+app.get('/submit-name',function(req,res){
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 
 
