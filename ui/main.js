ee55073ab6a90d0b1c493b4130981ac9e9d674da
SwethaGunta <SwethaGunta@users.noreply.github.com>
2017-08-12 16:55:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit_name.onclick = function(){
                 names = request.responseText;
                 names = JSON.parse(names);
                 var list = '';
-                for(var i=0;i<names.length();i++)
+                for(var i=0;i<names.length;i++)
                 {
                     list += '<li>' + names[i]  + '</li>';
                 }
