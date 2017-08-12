03588eb47794655ad954e86c3e5b331e4eb91829
SwethaGunta <SwethaGunta@users.noreply.github.com>
2017-08-12 12:22:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var span = 0;
+var element = document.getElementbyId('counter');
+element.onclick = function(){
+   span = span + 1;
+   var ele2 = document.getElementbyId('span');
+   ele2.innerHtml(span.toString());
+}
