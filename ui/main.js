8aa54ea98b67d76f355f48cb944eff8617ea09c2
SwethaGunta <SwethaGunta@users.noreply.github.com>
2017-08-26 15:33:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,5 @@
 console.log('Loaded!');
 
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-    //connecting to the counter endpoint
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState ===  XMLHttpRequest.DONE){
-            if(request.status === 200)
-            {
-                var count = request.responseText;
-                //console.log(count);
-                var span = document.getElementById('count');
-                span.innerHTML = count.toString();
-            }
-        }
-    }
-    request.open('GET',"http://svgunta.imad.hasura-app.io/counter",true);
-    request.send(null);
-   
-};
-
 var submit_name = document.getElementById('submitname');
 
 submit_name.onclick = function(){
