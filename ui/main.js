21f11758fafa9853f278ceed6806251693b5fa0e
SwethaGunta <SwethaGunta@users.noreply.github.com>
2017-08-26 15:23:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,7 +49,7 @@ submit_name.onclick = function(){
     var name = (document.getElementById('myname')).value;
     console.log(name);
     request.open('GET',"http://svgunta.imad.hasura-app.io/submit",true);
-   request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-request.send(name);
+//   request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+request.send(null);
   };
 
